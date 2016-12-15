8c6a831fdea08d2c833806b79407cebdb4251391
micahwierenga <wierenga.micah@gmail.com>
2016-12-14 18:50:33 -0700

    Still trying with heroku

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.createConnection('mongodb://localhost/memory-lane-app');
+
+// mongoose.createConnection('mongodb://localhost/memory-lane-app');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/memory-lane-app');
 
 
 module.exports.User = require('./user');
